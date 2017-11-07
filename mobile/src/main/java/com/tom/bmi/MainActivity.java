7e37613331ae7a89fdf4662f36dba73fc254b85f
boris10139914 <boris556655@gmail.com>
2017-11-07 11:23:12 +0800

    匿名類別 2017/11/17

--- mobile/src/main/java/com/tom/bmi/MainActivity.java
+++ mobile/src/main/java/com/tom/bmi/MainActivity.java
@@ -5,16 +5,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
 
+        }
+};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        getResources().getString(R.string.app_name);//"bmi"
+        Button btnHelp = (Button)findViewById(R.id.b_help);
+        btnHelp.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {//匿名類別
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("xxx")
+                        .setMessage("Bmi說明:.....")
+                        .setNeutralButton(R.string.OK,null
+                        )
+                        .show();
+
+            }
+        });}
+
     public  void bmi(View view){
         //Log.d("MainActivity","testing bmi method");
         EditText edWeight =(EditText)findViewById(R.id.ed_weight);
@@ -23,9 +43,9 @@ public class MainActivity extends AppCompatActivity {
         float height =Float.parseFloat(edHeight.getText().toString());
         float bmi = weight/(height*height);
 new AlertDialog.Builder(this)
-        .setMessage("Your BMI is:"+bmi)
-        .setTitle("HIHI")
-        .setNeutralButton("OK",null)//按ok之後不做事情+null
+        .setMessage(getString(R.string.Your_bmi_is)+bmi)
+        .setTitle(R.string.bmi_title)
+        .setNeutralButton(R.string.OK,null)//按ok之後不做事情+null
         .show();
        // Log.d("MainActivity","Your bmi is"+bmi);
        // Toast.makeText(this,"Your BMI is:"+bmi,Toast.LENGTH_LONG).show();
